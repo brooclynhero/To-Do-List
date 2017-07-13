1f77ffc33864c1811f6de6b07e66587d9d4b11a9
Philip Alekhin <phil.alekhin@gmail.com>
2017-07-13 17:54:55 +0300

    Refact code and move tableview logic into display manager

--- To Do List/TaskRowTableViewCell.swift
+++ To Do List/TaskRowTableViewCell.swift
@@ -13,15 +13,8 @@ class TaskRowTableViewCell: UITableViewCell {
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
     
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        
+    func setupWithTask(_ task: Task) {
+        nameLabel.text = task.nameOfTask
+        descriptionLabel.text = task.descriptionOfTask
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-    }
-
 }
--- To Do List/TaskRowTableViewController.swift
+++ To Do List/TaskRowTableViewController.swift
@@ -10,97 +10,53 @@ import UIKit
 
 class TaskRowTableViewController: UITableViewController {
     
+    var taskArrays = [Task]()
+    private let displayManager = TodoListDataDisplayManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.dataSource = displayManager
+        tableView.delegate = displayManager
+        
+        navigationItem.leftBarButtonItem = editButtonItem
+        
+        let firstTask = Task(name: "My first task", descr: "Some description")
+        let secondTask = Task(name: "My second task", descr: "Some description")
+        let thirdTask = Task(name: "My third task", descr: "Some description")
+        
+        displayManager.setupWithTasks([firstTask, secondTask, thirdTask])
+        tableView.reloadData()
+    }
+    
+    // MARK: IBActions
+    
     @IBAction func addButton(_ sender: UIBarButtonItem) {
         performSegue(withIdentifier: "oneSegue", sender: nil)
     }
     
-    var taskArrays = [Task]()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        
-        navigationItem.leftBarButtonItem = editButtonItem
-    
-
-        
-    }
-
-    override func didReceiveMemoryWarning() {
-        
-    }
-
-    
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        
-        return 1
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
-        return taskArrays.count
-        
-    }
-
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        let cellIdentifier = "TaskRowTableViewCell"
-        
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? TaskRowTableViewCell else {
-                fatalError("The dequeued cell is not an instance of TaskRowTableViewCell.")
-        }
-        
-        let taskName = taskArrays[indexPath.row]
-        
-        cell.nameLabel.text = taskName.nameOfTask
-        cell.descriptionLabel.text = taskName.descriptionOfTask
-        
- 
-    
-
-        return cell
-    }
-    
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let task = taskArrays[indexPath.row]
-        performSegue(withIdentifier: "oneSegue", sender: task)
-    }
- 
     @IBAction func unwindToTaskRows(sender: UIStoryboardSegue) {
         if let sourceViewController = sender.source as? SecondViewController, let task = sourceViewController.task {
             
             if let indexOfRow = sourceViewController.indexNumber {
-                
                 taskArrays[indexOfRow] = task
                 
                 if sourceViewController.needsDelete != nil {
                     taskArrays.remove(at: indexOfRow)
                 }
-            
-            
-            
-        } else {
-            let newIndexPath = IndexPath(row: taskArrays.count, section: 0)
-            taskArrays.append(task)
-            tableView.insertRows(at: [newIndexPath], with: .automatic)
+            } else {
+                let newIndexPath = IndexPath(row: taskArrays.count, section: 0)
+                taskArrays.append(task)
+                tableView.insertRows(at: [newIndexPath], with: .automatic)
             }
-            
         }
-        
         
         self.tableView.reloadData()
     }
-    
-    
-    
+
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        
         if segue.identifier == "oneSegue", let destinationVC = segue.destination as? SecondViewController {
-            
             if let task = sender as? Task {
-                
                 if let indexPath = self.tableView.indexPathForSelectedRow {
                     let indexPathValue = indexPath.row
                     destinationVC.indexNumber = indexPathValue
@@ -113,21 +69,4 @@ class TaskRowTableViewController: UITableViewController {
             }
         }
     }
-    
-    
-  
-
-    
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            
-            taskArrays.remove(at: indexPath.row)
-
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        }
-    }
-    
-
-    
-
 }
